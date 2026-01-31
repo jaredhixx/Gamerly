cc99df9c54de8041d421d454b672ec0f417f93cb
jaredhixx <jaredhixx@gmail.com>
2026-01-31 11:48:26 -0500

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,7 @@
+// /api/config.js
+export const CONFIG = {
+  RAWG_KEY: "ac669b002b534781818c488babf5aae4",
+  IGDB_CLIENT_ID: "7udxvmbguftujpbxqguez86hmzoe1a",
+  IGDB_CLIENT_SECRET: "qnc85s2tbevldz7qw205uuhot95kc5",
+  IGDB_ACCESS_TOKEN: "6mezrpfoq9ezakbxaf8msi6p94z7ut",
+};

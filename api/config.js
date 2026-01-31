6b4052820873a0cbf2183b487694eb9e73b68b78
jaredhixx <jaredhixx@gmail.com>
2026-01-31 17:45:09 -0500

    Delete api/config.js

--- api/config.js
+++
@@ -1,7 +0,0 @@
-// /api/config.js
-export const CONFIG = {
-  RAWG_KEY: "ac669b002b534781818c488babf5aae4",
-  IGDB_CLIENT_ID: "7udxvmbguftujpbxqguez86hmzoe1a",
-  IGDB_CLIENT_SECRET: "qnc85s2tbevldz7qw205uuhot95kc5",
-  IGDB_ACCESS_TOKEN: "8jhe81z0luj551j8buu1lil60f2sfr",
-};

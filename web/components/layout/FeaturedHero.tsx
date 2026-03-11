82397e1b75640a0c3dc9db36bde90297275469d8
Jared Hixx <jaredhixx@gmail.com>
2026-03-11 17:28:40 -0400

    set up links for images later

--- web/components/layout/FeaturedHero.tsx
+++ web/components/layout/FeaturedHero.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { buildGamePath } from "../../lib/site";
 import type { GamerlyGame } from "../../lib/igdb";
 

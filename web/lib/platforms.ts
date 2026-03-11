7bfff9107d626e2ad46b779e27ffb76b5bd83f65
Jared Hixx <jaredhixx@gmail.com>
2026-03-11 00:31:26 -0400

    replace favicon for new build

--- web/lib/platforms.ts
+++ web/lib/platforms.ts
@@ -4,26 +4,31 @@ export const platforms = {
     name: "PC Games",
     igdbIds: [6]
   },
+
   playstation: {
     slug: "playstation",
     name: "PlayStation Games",
     igdbIds: [48, 167]
   },
+
   xbox: {
     slug: "xbox",
     name: "Xbox Games",
     igdbIds: [49, 169]
   },
+
   switch: {
     slug: "switch",
     name: "Switch Games",
     igdbIds: [130]
   },
+
   ios: {
     slug: "ios",
     name: "iOS Games",
     igdbIds: [39]
   },
+
   android: {
     slug: "android",
     name: "Android Games",
